7d367acfa3d864345bb9edae4ee5a5d2b99ec681
soubj <sobujahmed124@gmail.com>
2023-09-18 20:06:14 +0600

    basic switch case and switch case with default

---
+++ SwitchCase.js
@@ -0,0 +1,46 @@
+// switch case
+var value = 4;
+// value is passes inside switch to compare with case
+switch (value) {
+  case 1:
+    console.log("alpha");
+    break;
+
+  case 2:
+    console.log("beta");
+    break;
+
+  case 3:
+    console.log("gamma");
+    break;
+
+  case 4:
+    console.log("x-ray");
+    break;
+}
+
+// --------------------------->>-------------------->>>
+// switch case with default statement
+value = "e";
+
+switch (value) {
+  case "a":
+    console.log("apple");
+    break;
+
+  case "b":
+    console.log("book");
+    break;
+
+  case "c":
+    console.log("cat");
+    break;
+
+  case "d":
+    console.log("dog");
+    break;
+
+  default:
+    console.log("nothing");
+    break;
+}
