cfe71196e4f7cdf52f240da166f2890b63e6f9d9
soubj <sobujahmed124@gmail.com>
2023-09-18 20:11:05 +0600

    showing one answer for multiple cases

--- SwitchCase.js
+++ SwitchCase.js
@@ -44,3 +44,32 @@ switch (value) {
     console.log("nothing");
     break;
 }
+
+// ------------------------>>------------------------------>>
+// one answer for multiple values or multiple cases
+value = 10;
+
+switch (value) {
+  case 1:
+  case 2:
+  case 3:
+    console.log("this answer is for case 1, 2 ,3");
+    break;
+  case 4:
+  case 5:
+  case 6:
+    console.log("this answer is for case 4, 5 ,6");
+    break;
+  case 7:
+  case 8:
+  case 9:
+    console.log("this answer is for case 7, 8 ,9");
+    break;
+  case 10:
+  case 11:
+  case 12:
+    console.log("this answer is for case 10, 11 ,12");
+    break;
+  default:
+    console.log("no value");
+}
