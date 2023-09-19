7a7576339757c44934c4069de6becbcc5fe93dec
soubj <sobujahmed124@gmail.com>
2023-09-19 20:55:15 +0600

    ternary operator nesting

---
+++ nestingTernary.js
@@ -0,0 +1,14 @@
+// normal ternary operator
+var val = 11;
+
+val === 10 ? console.log("ten") : console.log("not ten");
+
+// ------------------->>--------------------->>>
+// nesting ternary operator
+var num = 50;
+
+num > 50
+  ? console.log("more than 50")
+  : num < 50
+  ? console.log("num is less than 50")
+  : console.log("num is 50");
