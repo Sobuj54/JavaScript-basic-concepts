9703390ebb77a02aaf372ebc565f3fd18bc27587
soubj <sobujahmed124@gmail.com>
2023-09-23 23:25:28 +0600

    reverse string in place

--- stringReverse.js
+++ stringReverse.js
@@ -14,10 +14,19 @@ console.log(reverse("sobuj ahmed"));
 
 // ---------------------->>>---------------------->>
 // using built in methods to reverse string
-const str = "sobuj";
-const reversedStr = str.split("").reverse().join("");
-console.log("using built in methods :", reversedStr);
+// const str = "sobuj";
+// const reversedStr = str.split("").reverse().join("");
+// console.log("using built in methods :", reversedStr);
 function builtInReverse(str) {
   return str.split("").reverse().join("");
 }
 console.log(builtInReverse("name"));
+
+// ------------------->>>----------------------->>
+// reverse string in place for ex:
+// "I am the good boy" will be "I ma eht doog yob"
+
+function revInPlace(str) {
+  return str.split(" ").reverse().join(" ").split("").reverse().join("");
+}
+console.log(revInPlace("I am the good boy"));
