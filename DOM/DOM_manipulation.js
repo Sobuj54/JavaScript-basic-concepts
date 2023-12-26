45a303d676063ec4652e3611fbec5d73a4eb26a3
soubj <sobujahmed124@gmail.com>
2023-12-26 22:38:46 +0600

    selecting parent,children and sibling element

--- DOM/DOM_manipulation.js
+++ DOM/DOM_manipulation.js
@@ -17,3 +17,19 @@ console.log(getName);
 
 const getProfession = document.querySelectorAll(".pro"); //getting by class
 console.log(getProfession);
+
+// -------------
+// getting parent element or parent node
+const parent = document.querySelector("#name").parentNode; //.parentElement for parent element
+console.log("parent element:", parent);
+
+// getting sibling elements
+const sibling = document.querySelector(".pro").nextElementSibling; //gives next sibling
+console.log("next siblings", sibling);
+
+const previousSibling = document.querySelector(".pro").previousElementSibling;
+console.log("previous siblings:", previousSibling);
+
+// get child nodes
+const child = document.querySelector(".wrapper").childNodes; //.children to get child elements
+console.log("child nodes", child);
