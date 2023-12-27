6959a136a208a171d85f85194b2ece121389739b
soubj <sobujahmed124@gmail.com>
2023-12-27 23:13:11 +0600

    keypress and mouseover event listener

--- DOM/eventListeners.js
+++ DOM/eventListeners.js
@@ -14,3 +14,17 @@ form.addEventListener("submit", (e) => {
   const input = wholeForm.input.value; // getting value of the input field whose name attribute is input
   console.log(input);
 });
+
+// --------->>--------------->>
+// keypress event listener
+const input = document.getElementById("alone-input");
+input.addEventListener("keypress", (e) => {
+  console.log(e.key);
+});
+
+// ------------------->>------------------>>
+// mouseover event listener
+const developer = document.getElementById("my-name");
+developer.addEventListener("mouseover", () => {
+  developer.style.color = "orange";
+});
